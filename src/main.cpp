7104c17db84d6119ad880f353ca60cb99fbc0bea
ATX-PRAGMA <leviathaninwaves@icloud.com>
2020-03-18 15:58:40 -0500

    Test complete Primary randomization

--- src/main.cpp
+++ src/main.cpp
@@ -9,58 +9,7 @@
 #include "LMG.h"
 #include "SR.h"
 
-/* 
-SMG
-MSMC, RUS-79U, PDW-57/Razorback, Pharo, Chicom, HG 40
-Iron Sight, Reflex sight, Holographic Sight
-None, Quickdraw, Stock, Extended Mag, Fast Mag, Foregrip, Suppressor, 
-Long Barrel, Laser Sight, FMJ
 
-AR
-AK117, AK-47, ASM10, BK57, HBRa3, ICR-1, KN-44, LK24, M16, M4, Man-O-War
-Type 25.
-Iron Sight, Reflex Sight, Holographic Sight, Tactical Scope
-None, Stock, Laser Sight, Quickdraw, FMJ, Suppressor, Foregrip, Fast Mag, Extended Mag
-
-Shotgun
-BY15, HS0405, HS2126, KRM-262, Striker.
-Iron sight.
-None, Laser Sight, Long Barrel, Fast Mag, Suppressor, Extended Mag
-
-LMG
-M4LMG, RPD, S36, UL376.
-Iron Sight, Reflex Sight, Holographic Sight, Tactical Scope
-None, Stock, Laser Sight, Quickdraw, FMJ, Suppressor, Foregrip, Fast Mag, Extended Mag
-
-Sniper
-Arctic .50, DL Q33, Locus, M21 EBR, XPR-50
-Default Scope, Tactical Scope
-None, Stock, FMJ, Fast Mag, Suppresor, Extended Mag
-
-Secondary
-Axe, Baseball bat, FHJ-18, Ice Axe, J358, Karambit, Knife, MW11, SMRS
-J358 & MW11 Attachments:
-Iron Sight
-None, Laser Sight, FMJ, Fast Mag, Suppressor, Extended Mag
-
-Operator Skills
-Tempest, Sparrow, Gravity Spikes, H.I.V.E, War Machine, Purifier, 
-Transform Shield, Death Machine/Scythe
-
-Lethal
-Frag Grenade, Trip Mine, Sticky Grenade, Combat Axe
-
-Tactical
-Smoke Grenade, Flashbang Grenade, Concussion Grenade, EMP, Trophy System
-
-Perks
-RED:
-Lightweight, Fast Recover, Persistence, Flak Jacket, Skulker, Agile
-GREEN:
-Hard Wired, Vulture, Toughness, Tracker, Ghost, Cold-Blooded
-BLUE:
-Dead Silence, Hardline, Demo Expert, Tactical Mask, Alert, Engineer
-*/
 
 int main() {
 
@@ -68,9 +17,32 @@ int main() {
 	PrimWeapon *Primary = new PrimWeapon();
 	//SecWeapon *Secondary = new Secondary();
 	int prim_list_index;
-	std::string test;
+
 	prim_list_index = ((int) generateRandomNumber(0, (double) Primary->prim_weapon_list_end));
-	test = Primary->prim_weapon_list[prim_list_index];
-	std::cout << test << std::endl;
+	
+	
+	//establish which category the randomly selected primary belongs to and cerate the appropriate object
+	if ((prim_list_index >= Primary->smg_bounds[0]) && (prim_list_index <= Primary->smg_bounds[1]))
+	{ SMG *PrimaryWeap = new SMG(); }
+	elseif ((prim_list_index >= Primary->ar_bounds[0]) && (prim_list_index <= Primary->ar_bounds[1]))
+	{ AR *PrimaryWeap = new AR(); }
+	elseif ((prim_list_index >= Primary->sg_bounds[0]) && (prim_list_index <= Primary->sg_bounds[1]))
+	{ SG *PrimaryWeap = new SG(); }
+	elseif ((prim_list_index >= Primary->lmg_bounds[0]) && (prim_list_index <= Primary->lmg_bounds[1]))
+	{ LMG *PrimaryWeap = new LMG(); }
+	elseif ((prim_list_index >= Primary->sr_bounds[0]) && (prim_list_index <= Primary->sr_bounds[1]))
+	{ SR *PrimaryWeap = new SR(); }
+	
+	PrimaryWeap->setSights();
+	PrimaryWeap->setAttachments();
+	
+	PrimaryWeap->weapon_name = Primary->prim_weapon_list[prim_list_index];
+	std::cout << PrimaryWeap->weapon_name << "\n" 
+	          << PrimaryWeap->sight << "\n" 
+	          << PrimaryWeap->first_attach << "\n"
+	          << PrimaryWeap->second_attach << "\n"
+	          << PrimaryWeap->third_attach << "\n"
+	          << std::endl;
+	
 	return 0;
 }
